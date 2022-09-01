bb5cef4b41f6e3564a833ee99bc7308b02b779d7
equiman <equiman@users.noreply.github.com>
2022-09-01 12:21:39 -0500

    solved firts six

--- src/06-unions.problem.ts
+++ src/06-unions.problem.ts
@@ -1,20 +1,33 @@
-interface User {
-  id: number;
-  firstName: string;
-  lastName: string;
-  /**
-   * How do we ensure that role is only one of:
-   * - 'admin'
-   * - 'user'
-   * - 'super-admin'
-   */
-  role: string;
-}
+
+// type Role = ('admin' | 'user' | 'super-admin')
+
+// interface User {
+//   id: number;
+//   firstName: string;
+//   lastName: string;
+//   role: Role;
+// }
+
+type RoleAttributes = (| { role: "admin", password: string } | { role: "user" } | {role: "super-admin", password: string})
+
+type User = {
+  id: number,
+  firstName: string,
+  lastName: string
+} & RoleAttributes /* Discriminated Union Type */
+
+type Role = RoleAttributes['role'] /* Derived Type */
 
 export const defaultUser: User = {
   id: 1,
   firstName: "Matt",
   lastName: "Pocock",
-  // @ts-expect-error
-  role: "I_SHOULD_NOT_BE_ALLOWED",
-};
+  role: "admin",
+  password: 'p4ssw0rd'
+}
+
+const logRole = (role: Role) =>{ 
+  console.log(role)
+}
+
+logRole('user')
